856dcbe278e9df3cc8b995b696e802b25efb0771
Joseph Khan <joseph.khan@altayyargroup.com>
2020-03-25 23:30:32 +0400

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+/** 
+ * Sum of two number
+ * @param a {Number}
+ * @param b {number}
+ * @returns {Number}
+*/
+const sum = (a, b) => {
+    return a + b;
+}
+
+/** 
+ * Multiply two numbers
+ * @param a {Number}
+ * @param b {number}
+ * @returns {Number}
+*/
+const multiply = (a, b) => {
+    return a * b;
+}
+
+module.exports = {
+    sum: sum,
+    multiply: multiply
+}
