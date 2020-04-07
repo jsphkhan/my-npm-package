a5ddd1acb69858d528b5e3a3445fe225f7475893
Joseph Khan <joseph.khan@altayyargroup.com>
2020-04-07 16:55:41 +0400

    dist folder

--- dist/index.js
+++ dist/index.js
@@ -53,11 +53,22 @@ var divide = function divide(a, b) {
 var remainder = function remainder(a, b) {
   return a % b;
 };
+/** 
+ * Square function
+ * @param a {Number}
+ * @returns {Number}
+*/
+
+
+var square = function square(a) {
+  return a * a;
+};
 
 module.exports = {
   sum: sum,
   multiply: multiply,
   subtract: subtract,
   divide: divide,
-  remainder: remainder
+  remainder: remainder,
+  square: square
 };
