5cb0054ac7e076418dd87d35011e85ea13e6fed8
Joseph <jsphkhan@gmail.com>
2022-06-29 21:37:57 +0400

    added babel config

---
+++ dist/utils.js
@@ -0,0 +1,15 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.utilMultuply = utilMultuply;
+exports.utilSum = utilSum;
+
+function utilSum(a, b) {
+  return a = b;
+}
+
+function utilMultuply(a, b) {
+  return a * b;
+}
---
+++ src/utils.js
@@ -0,0 +1,7 @@
+export function utilSum(a, b) {
+    return a = b;
+}
+
+export function utilMultuply(a, b) {
+    return a * b;
+}
