4b056f609b51cd6f8f74917e925bab3972f1cf1d
Joseph <jsphkhan@gmail.com>
2022-08-09 11:32:59 +0400

    feat: new build

--- dist/index.js
+++ dist/index.js
@@ -1,63 +1,147 @@
 "use strict";
-/** 
- * Sum of two number
- * @param a {Number}
- * @param b {number}
- * @returns {Number}
-*/
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.sum = exports.subtract = exports.sleep = exports.multiply = exports.isValidName = exports.isProdEnv = exports.goToMapURL = exports.getUrlFromQuery = exports.getFixedValue = exports.getCountryCodePrefix = exports.checkLength = void 0;
+
+var _size2 = _interopRequireDefault(require("lodash/size"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+/**
+ * Common Utils
+ */
+var REGEX = {
+  VALID_NAME: /^[\u0621-\u064A\u0660-\u0669a-zA-Z]+([\s][\u0621-\u064A\u0660-\u0669a-zA-Z]+)*$/
+};
 
 var sum = function sum(a, b) {
+  console.log('** sum');
   return a + b;
 };
-/** 
- * Multiply two numbers
- * @param a {Number}
- * @param b {number}
- * @returns {Number}
-*/
 
+exports.sum = sum;
 
 var multiply = function multiply(a, b) {
+  console.log('** multiply');
   return a * b;
 };
-/** 
- * Subtract two numbers
- * @param a {Number}
- * @param b {number}
- * @returns {Number}
-*/
 
+exports.multiply = multiply;
 
 var subtract = function subtract(a, b) {
+  console.log('** subtract');
   return a - b;
 };
-/** 
- * Divide two numbers
- * @param a {Number}
- * @param b {number}
+/**
+ * return fixed number
+ * @param {Number} value
+ * @param {Number} fixedBy
  * @returns {Number}
-*/
+ */
 
 
-var divide = function divide(a, b) {
-  return a / b;
+exports.subtract = subtract;
+
+var getFixedValue = function getFixedValue(value, fixedBy) {
+  if (Number(value)) {
+    return Number(value).toFixed(fixedBy);
+  }
+
+  return 0;
 };
-/** 
- * Modulus
- * @param a {Number}
- * @param b {number}
- * @returns {Number}
-*/
+/**
+ * return a resolve promise after given time interval
+ * @param {Number} ms
+ * @returns {Promise}
+ */
 
 
-var remainder = function remainder(a, b) {
-  return a % b;
+exports.getFixedValue = getFixedValue;
+
+var sleep = function sleep(ms) {
+  return new Promise(function (resolve) {
+    return setTimeout(resolve, ms);
+  });
+};
+/**
+ * Checks length of passed text is it is more than 3
+ * @param {string} text
+ * @returns {boolean}
+ */
+
+
+exports.sleep = sleep;
+
+var checkLength = function checkLength(text) {
+  return (0, _size2["default"])(text) >= 3;
+};
+/**
+ * Checks if it is dev or prod environment
+ * @returns {Boolean}
+ */
+
+
+exports.checkLength = checkLength;
+
+var isProdEnv = function isProdEnv() {
+  // prod env can be dev, stage or prod server eg. npm start
+  // local is local dev env eg. npm run dev
+  return process.env.NODE_ENV === 'production';
+};
+/**
+ * get URL from queryObject
+ * @param {Object} queryObj
+ * @returns {String}
+ */
+
+
+exports.isProdEnv = isProdEnv;
+
+var getUrlFromQuery = function getUrlFromQuery(queryObj) {
+  return Object.keys(queryObj).map(function (k) {
+    return "".concat(k, "=").concat(queryObj[k]);
+  }).join('&');
+};
+/**
+ * go to map location for specific map url
+ * @param {String} mapUrl
+ */
+
+
+exports.getUrlFromQuery = getUrlFromQuery;
+
+var goToMapURL = function goToMapURL(mapUrl) {
+  window.open(mapUrl);
+};
+/**
+ * Return false is value is invalid.
+ * @param {String} field
+ * @returns {Boolean}
+ */
+
+
+exports.goToMapURL = goToMapURL;
+
+var isValidName = function isValidName(field) {
+  console.log('** isValidName');
+  return REGEX.VALID_NAME.test(field);
+};
+/**
+ * Return the country code prefix for selected language
+ *  for en language eg: 966+
+ * for ar language eg: +966
+ * @param {String} countryCode
+ * @param {Boolean} isArabic
+ * @returns {String}
+ */
+
+
+exports.isValidName = isValidName;
+
+var getCountryCodePrefix = function getCountryCodePrefix(countryCode, isArabic) {
+  return isArabic ? "".concat(countryCode, "+") : "+".concat(countryCode);
 };
 
-module.exports = {
-  sum: sum,
-  multiply: multiply,
-  subtract: subtract,
-  divide: divide,
-  remainder: remainder
-};
+exports.getCountryCodePrefix = getCountryCodePrefix;
--- src/index.js
+++ src/index.js
@@ -1,59 +1,107 @@
-/** 
- * Sum of two number
- * @param a {Number}
- * @param b {number}
- * @returns {Number}
-*/
-const sum = (a, b) => {
+/**
+ * Common Utils
+ */
+
+import _size from 'lodash/size';
+
+const REGEX = {
+  VALID_NAME:
+    /^[\u0621-\u064A\u0660-\u0669a-zA-Z]+([\s][\u0621-\u064A\u0660-\u0669a-zA-Z]+)*$/,
+};
+
+export const sum = (a, b) => {
+    console.log('** sum');
     return a + b;
 }
 
-/** 
- * Multiply two numbers
- * @param a {Number}
- * @param b {number}
- * @returns {Number}
-*/
-const multiply = (a, b) => {
+export const multiply = (a, b) => {
+    console.log('** multiply');
     return a * b;
 }
 
-
-/** 
- * Subtract two numbers
- * @param a {Number}
- * @param b {number}
- * @returns {Number}
-*/
-const subtract = (a, b) => {
+export const subtract = (a, b) => {
+    console.log('** subtract');
     return a - b;
 }
 
-
-/** 
- * Divide two numbers
- * @param a {Number}
- * @param b {number}
+/**
+ * return fixed number
+ * @param {Number} value
+ * @param {Number} fixedBy
  * @returns {Number}
-*/
-const divide = (a, b) => {
-    return a / b;
-}
+ */
 
-/** 
- * Modulus
- * @param a {Number}
- * @param b {number}
- * @returns {Number}
-*/
-const remainder = (a, b) => {
-    return a % b;
-}
+export const getFixedValue = (value, fixedBy) => {
+  if (Number(value)) {
+    return Number(value).toFixed(fixedBy);
+  }
+  return 0;
+};
 
-module.exports = {
-    sum: sum,
-    multiply: multiply,
-    subtract: subtract,
-    divide: divide,
-    remainder: remainder
-}
+/**
+ * return a resolve promise after given time interval
+ * @param {Number} ms
+ * @returns {Promise}
+ */
+export const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
+/**
+ * Checks length of passed text is it is more than 3
+ * @param {string} text
+ * @returns {boolean}
+ */
+export const checkLength = (text) => {
+  return _size(text) >= 3;
+};
+
+/**
+ * Checks if it is dev or prod environment
+ * @returns {Boolean}
+ */
+export const isProdEnv = () => {
+  // prod env can be dev, stage or prod server eg. npm start
+  // local is local dev env eg. npm run dev
+  return process.env.NODE_ENV === 'production';
+};
+
+/**
+ * get URL from queryObject
+ * @param {Object} queryObj
+ * @returns {String}
+ */
+export const getUrlFromQuery = (queryObj) => {
+  return Object.keys(queryObj)
+    .map((k) => `${k}=${queryObj[k]}`)
+    .join('&');
+};
+
+/**
+ * go to map location for specific map url
+ * @param {String} mapUrl
+ */
+
+export const goToMapURL = (mapUrl) => {
+  window.open(mapUrl);
+};
+
+/**
+ * Return false is value is invalid.
+ * @param {String} field
+ * @returns {Boolean}
+ */
+export const isValidName = (field) => {
+  console.log('** isValidName');
+  return REGEX.VALID_NAME.test(field);
+};
+
+/**
+ * Return the country code prefix for selected language
+ *  for en language eg: 966+
+ * for ar language eg: +966
+ * @param {String} countryCode
+ * @param {Boolean} isArabic
+ * @returns {String}
+ */
+export const getCountryCodePrefix = (countryCode, isArabic) => {
+  return isArabic ? `${countryCode}+` : `+${countryCode}`;
+};
